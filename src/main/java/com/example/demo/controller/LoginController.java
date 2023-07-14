8f0e2bd729e86f697cf272d2c7eaa3790e2c0e88
frnkfork <sukexd1@gmail.com>
2023-07-14 05:03:55 -0500

    :sparkles: Añadir Controladores

---
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.demo.controller;
+
+
+import com.example.demo.model.Professor;
+import com.example.demo.model.User;
+import com.example.demo.services.ProfessorService;
+import com.example.demo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class LoginController 
+{
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private ProfessorService professorService;
+	
+	@GetMapping("/")
+    public String welcomeMessage()
+    {
+    	return "Welcome to Elearning Management system !!!";
+    }
+	
+	@PostMapping("/loginuser")
+	@CrossOrigin(origins = "http://localhost:8080")
+	public User loginUser(@RequestBody User user) throws Exception
+	{
+		String currEmail = user.getEmail();
+		String currPassword = user.getPassword();
+		
+		User userObj = null;
+		if(currEmail != null && currPassword != null)
+		{
+			userObj = userService.fetchUserByEmailAndPassword(currEmail, currPassword);
+		}
+		if(userObj == null)
+		{
+			throw new Exception("User does not exists!!! Please enter valid credentials...");
+		}		
+		return userObj;
+	}
+	
+	@PostMapping("/loginprofessor")
+	@CrossOrigin(origins = "http://localhost:8080")
+	public Professor loginDoctor(@RequestBody Professor professor) throws Exception
+	{
+		String currEmail = professor.getEmail();
+		String currPassword = professor.getPassword();
+		
+		Professor professorObj = null;
+		if(currEmail != null && currPassword != null)
+		{
+			professorObj = professorService.fetchProfessorByEmailAndPassword(currEmail, currPassword);
+		}
+		if(professorObj == null)
+		{
+			throw new Exception("Professor does not exists!!! Please enter valid credentials...");
+		}		
+		return professorObj;
+	}
+}
---
+++ src/main/java/com/example/demo/controller/RegistrationController.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.demo.controller;
+
+
+import com.example.demo.model.Professor;
+import com.example.demo.model.User;
+import com.example.demo.services.ProfessorService;
+import com.example.demo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class RegistrationController 
+{
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private ProfessorService professorService;
+	 @CrossOrigin(origins = "http://localhost:8080")
+	@PostMapping("/registeruser")
+
+	public User registerUser(@RequestBody User user) throws Exception
+	{
+		String currEmail = user.getEmail();
+		String newID = getNewID();
+		user.setUserid(newID);
+		
+		if(currEmail != null || !"".equals(currEmail))
+		{
+			User userObj = userService.fetchUserByEmail(currEmail);
+			if(userObj != null)
+			{
+				throw new Exception("User with "+currEmail+" already exists !!!");
+			}
+		}
+		User userObj = null;
+		userObj = userService.saveUser(user);
+		return userObj;
+	}
+	
+	@PostMapping("/registerprofessor")
+	@CrossOrigin(origins = "http://localhost:8080")
+	public Professor registerDoctor(@RequestBody Professor professor) throws Exception
+	{
+		String currEmail = professor.getEmail();
+		String newID = getNewID();
+		professor.setProfessorid(newID);
+		
+		if(currEmail != null || !"".equals(currEmail))
+		{
+			Professor professorObj = professorService.fetchProfessorByEmail(currEmail);
+			if(professorObj != null)
+			{
+				throw new Exception("Professor with "+currEmail+" already exists !!!");
+			}
+		}
+		Professor professorObj = null;
+		professorObj = professorService.saveProfessor(professor);
+		return professorObj;
+	}
+	
+	public String getNewID()
+	{
+		String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"+"0123456789"+"abcdefghijklmnopqrstuvxyz";
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 12; i++) 
+        {
+            int index = (int)(AlphaNumericString.length() * Math.random());
+            sb.append(AlphaNumericString.charAt(index));
+        }
+        return sb.toString();
+	}
+}
